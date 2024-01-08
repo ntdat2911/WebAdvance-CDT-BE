f5f3be0839febabc285f7e9c8dd4bf4191c51856
CHIẾN HUỲNH MINH <huynhchien293@gmail.com>
2024-01-08 20:10:07 +0700

    fix: Delete socket

--- index.js
+++ index.js
@@ -9,31 +9,15 @@ const userRoutes = require("./components/users");
 const verifyRoutes = require("./components/verify");
 const adminRoutes = require("./components/admin");
 const classRoutes = require("./components/class");
-const { Server } = require("socket.io");
-// const { createAdapter } = require("@socket.io/redis-adapter");
-// const { createClient } = require("redis");
-const classRepository = require("./components/class/ClassRepository");
 
-const middleware = require("./middleware/auth");
 const { createServer } = require("http");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const CLIENT_HOST = process.env.CLIENT_HOST || "http://localhost:3000";
-let onlineUsers = [];
-const socketPort = process.env.SOCKET_PORT || 3500;
 
 require("dotenv").config();
 
-// const pubClient = createClient({ host: "localhost", port: 6379 });
-// const subClient = pubClient.duplicate();
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: CLIENT_HOST,
-    methods: ["GET", "POST"],
-  },
-});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -59,64 +43,6 @@ app.use(
   })
 );
 
-const addNewUser = (userId, socketId) => {
-  !onlineUsers.some((user) => user.userId === userId) &&
-    onlineUsers.push({ userId, socketId });
-};
-
-const removeUser = (id) => {
-  onlineUsers = onlineUsers.filter((user) => user.userId !== id);
-};
-
-const getUser = (userId) => {
-  return onlineUsers.find((user) => user.userId === userId);
-};
-
-const newNotiList = async (userId) => {
-  const result = await classRepository.getNotifications(userId);
-  console.log(result, " chuan bi gui");
-  return result;
-};
-let count = 0;
-io.on("connection", (socket) => {
-  // let myinterval = setInterval(function() {
-  //   console.log("New connection ", onlineUsers);
-  // }, 3000);
-  // myinterval
-  count++;
-  console.log("connected: ", count);
-  socket.on("newUser", (userId) => {
-    addNewUser(userId, socket.id);
-    console.log("Online users: ", onlineUsers);
-  });
-
-  socket.on("sendClassNotification", async ({ data }) => {
-    console.log(data, " CLASS NOTI DATA");
-    for (const rev of data.receiverId) {
-      console.log(rev.userId);
-      const receiver = getUser(rev.userId);
-      console.log(receiver);
-      if (receiver) {
-        io.to(receiver.socketId).emit("getNotification", {
-          content: await newNotiList(receiver.userId),
-        });
-      }
-    }
-  });
-
-  socket.on("sendNotification", async ({ senderId, receiverId, type }) => {
-    const receiver = getUser(receiverId);
-    console.log(type);
-    io.to(receiver.socketId).emit("getNotification", {
-      content: await newNotiList(receiver.userId),
-    });
-  });
-
-  socket.on("disconnect", (userId) => {
-    console.log("USER disconnect ", onlineUsers);
-    removeUser(userId);
-  });
-});
 
 //app.use(passport.authenticate('session'));
 app.use(passport.initialize());
@@ -126,8 +52,6 @@ app.use("/", userRoutes);
 app.use("/verify", verifyRoutes);
 app.use("/admin", adminRoutes);
 app.use("/class", classRoutes);
-
-io.listen(socketPort, () => console.log(`Socket running at ${socketPort}`));
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
 
